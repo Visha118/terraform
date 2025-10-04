aa90cc93b752e28b842866fe7a3f4cf1b897ee16
Visha118 <v.recess@gmail.com>
2025-10-04 22:36:28 +0530

    vpc, subnet, security group, route tables added

--- main.tf
+++ main.tf
@@ -1,10 +1,112 @@
+#creating vpc
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+#creating subnets
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "private-subnet"
+  }
+}
+
+#Attach IGW to VPC (needed for internet access).
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+#create route tables
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public-rt"
+  }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_security_group" "public_sg" {
+  name        = "public-ec2-sg"
+  vpc_id      = aws_vpc.main.id
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # 👈 open SSH from anywhere (ok for testing)
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "public-ec2-sg"
+  }
+}
+
 resource "aws_instance" "my_first_instance" {
   ami           = "ami-0c94855ba95c71c99"
   instance_type = "t2.micro"
   key_name      = aws_key_pair.generated_key.key_name
+  subnet_id              = aws_subnet.public.id
 
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
-  # associate_public_ip_address = true
+  vpc_security_group_ids = [aws_security_group.public_sg.id]
+  associate_public_ip_address = true
+  user_data = <<-EOF
+            #!/bin/bash
+            yum update -y
+            yum install -y httpd
+            systemctl start httpd
+            systemctl enable httpd
+            echo "<h1>Hello from Vishal's web server. You are valued here.</h1>" > /var/www/html/index.html
+            EOF
 
   tags = {
     Name = "MyFirstTerraformInstance"
@@ -27,21 +129,17 @@ resource "local_file" "private_key_pem" {
   file_permission = "0400"
 }
 
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = "Allow SSH inbound traffic"
+#ec2 - private subnet
+resource "aws_instance" "private_ec2" {
+  ami           = "ami-0c94855ba95c71c99"
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.generated_key.key_name
+  subnet_id              = aws_subnet.private.id
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  vpc_security_group_ids = [aws_security_group.public_sg.id]
+  # associate_public_ip_address = true
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  tags = {
+    Name = "MyFirstTerraformInstance"
   }
 }
