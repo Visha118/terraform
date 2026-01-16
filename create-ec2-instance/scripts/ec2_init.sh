e5e85f565395e2ffe4158efb065654813d20f94d
Visha118 <v.recess@gmail.com>
2026-01-16 20:11:36 +0530

    kubectl and terraform installation steps added

--- create-ec2-instance/scripts/ec2_init.sh
+++ create-ec2-instance/scripts/ec2_init.sh
@@ -4,6 +4,7 @@
 exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 
 # installing docker
+echo "********* Docker installation starts! *********"
 # Add Docker's official GPG key:
 sudo apt update
 sudo apt install -y ca-certificates curl
@@ -25,4 +26,26 @@ sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin d
 # Start Docker and allow ec2-user/ubuntu to run it without sudo
 sudo systemctl enable --now docker
 sudo usermod -aG docker ubuntu
-echo "Docker installation complete!"
+echo "********* Docker installation complete! *********"
+
+#Install kubectl utility
+echo "********* Kubectl installation starts! *********"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+echo "********* Kubectl installation complete! *********"
+
+#Install terraform
+echo "********* Terraform installation starts! *********"
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+gpg --dearmor | \
+sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+gpg --no-default-keyring \
+--keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+--fingerprint
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update
+sudo apt-get install terraform
+echo "********* Terraform installation complete! *********"
