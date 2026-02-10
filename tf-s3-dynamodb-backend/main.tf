e61b516cfd9bb1134779e630ad67af947bc017eb
Visha118 <v.recess@gmail.com>
2026-02-10 11:38:30 +0530

    s3 backend setup code added

---
+++ tf-s3-dynamodb-backend/main.tf
@@ -0,0 +1,37 @@
+#create s3 bucket - where we will terraform state file remotely.
+resource "aws_s3_bucket" "this" {
+  bucket = "tf-s3-state-eks-bucket"
+
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+resource "aws_s3_bucket_versioning" "this" {
+  bucket = aws_s3_bucket.this.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+#create dynamodb table for state file locking.
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-state-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
